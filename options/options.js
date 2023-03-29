0e9dbb9521ceb0a9667cb50b1cecf2cfcf64154f
Saurav <sp.gharti@gmail.com>
2023-03-29 16:04:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-mountain-2",
+  url: "https://tight-mountain-2.botics.co",
+  api: "https://tight-mountain-2.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
